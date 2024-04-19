60843c7d6e5f3f16e7067408b690200cc4a4ce56
mark_le <mark.mqle@gmail.com>
2024-04-18 21:57:19 -0700

    Finish main page CyberSneaker

--- model/Shoes.js
+++ model/Shoes.js
@@ -1,1 +1,13 @@
-//chỉnh responsive chung cả web
+//class for shoes (from API)
+class Shoe{
+    // phuong thuc khoi tao
+    constructor(name, price, description, shortDescription, size, categories, image){
+        this.name = name
+        this.price = price
+        this.description = description
+        this.shortDescription = shortDescription
+        this.size = size
+        this.categories = categories
+        this.image = image
+    }
+}
