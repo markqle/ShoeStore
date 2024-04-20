84f415a85268a47a6bf2d9b9e1df3679cc8458f9
mark_le <mark.mqle@gmail.com>
2024-04-20 05:29:57 -0700

    register form

---
+++ model/User.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(InputPhone, InputName, InputGender, InputEmail, InputPassword){
+        this.InputPhone = InputPhone
+        this.InputName = InputName
+        //TODO how to dom to Gender (either radio / dropdown)
+        this.gender = InputGender
+        this.InputEmail = InputEmail
+        this.InputPassword = InputPassword
+    }
+}
